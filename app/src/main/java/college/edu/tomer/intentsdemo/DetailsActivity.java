1675d08b4e98491b3fd2c40b59b152f23db73013
iAndroidCollege <collegeandroid2015@gmail.com>
2016-05-22 20:57:27 +0300

    added intents

--- app/src/main/java/college/edu/tomer/intentsdemo/DetailsActivity.java
+++ app/src/main/java/college/edu/tomer/intentsdemo/DetailsActivity.java
@@ -1,7 +1,11 @@
 package college.edu.tomer.intentsdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import models.PlayList;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Intent dIntent = getIntent();
+        int songIndex = dIntent.getIntExtra("SongIndex", -1);
+
+        Toast.makeText(DetailsActivity.this, PlayList.songs.get(songIndex).getTitle(), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/college/edu/tomer/intentsdemo/MainActivity.java
+++ app/src/main/java/college/edu/tomer/intentsdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package college.edu.tomer.intentsdemo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewParent;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,6 +32,33 @@ public class MainActivity extends AppCompatActivity {
         song2 = new SongViewHolder(findViewById(R.id.song2), songs.get(1));
         song3 = new SongViewHolder(findViewById(R.id.song3), songs.get(2));
         song4 = new SongViewHolder(findViewById(R.id.song4), songs.get(3));
+    }
+
+    public void showDetails(View view) {
+        //view.setBackgroundColor(0xFF0000FF);
+        //1) instantiate a new Intent with the destination class
+        ViewParent parent = view.getParent();
+        Context c = getApplicationContext();
+        Intent dIntent = new Intent(c, DetailsActivity.class);
+
+        //put extra data
+        switch (view.getId()){
+            case R.id.song1:
+                dIntent.putExtra("SongIndex", 0);
+                break;
+            case R.id.song2:
+                dIntent.putExtra("SongIndex", 1);
+                break;
+            case R.id.song3:
+                dIntent.putExtra("SongIndex", 2);
+                break;
+            case R.id.song4:
+                dIntent.putExtra("SongIndex", 3);
+                break;
+        }
+
+        //3) start the intent:
+        startActivity(dIntent);
     }
 }
 class SongViewHolder{
